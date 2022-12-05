0c2637f796fe94c3a33297f4748f5e4067a0b730
Daniel López Lacalle <daniel.lopez@bitboxonline.com>
2022-12-05 09:08:17 +0000

    Adds an empty home screen widget

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sailing_speed/screens/home.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -6,22 +8,13 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sailing Speed',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(
-        color: Colors.white,
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Sailing Speed',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomeScreen(),
       ),
     );
   }

6ef8a7b3efb1472a9b7a134fb76cdaabf4325772
Daniel López Lacalle <daniel.lopez@bitboxonline.com>
2022-12-05 08:59:13 +0000

    Some more cleanup of default project

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,9 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: Container(
+        color: Colors.white,
+      ),
     );
   }
 }
